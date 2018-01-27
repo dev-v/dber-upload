8205be99a1f91724b01a26d753e26fe6501764b3
dev-v <250912570@qq.com>
2018-01-27 22:29:20 +0800

    登录实体提交

--- dber-upload-service/src/main/java/com/dber/upload/config/UploadWebConfig.java
+++ dber-upload-service/src/main/java/com/dber/upload/config/UploadWebConfig.java
@@ -1,24 +1,23 @@
 package com.dber.upload.config;
 
-import com.dber.plat.api.PlatLoginHelper;
+import com.dber.base.config.BaseWebConfig;
+import com.dber.upload.web.UploadLoginHelper;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-
-import com.dber.base.config.BaseWebConfig;
 
 /**
  * <li>文件名称: WebConfig.java</li>
  * <li>修改记录: ...</li>
  * <li>内容摘要: ...</li>
  * <li>其他说明: ...</li>
- * 
+ *
+ * @author dev-v
  * @version 1.0
  * @since 2017年12月21日
- * @author dev-v
  */
 @Configuration
-@Import({ UploadServiceConfig.class,BaseWebConfig.class, PlatLoginHelper.class})
+@Import({UploadServiceConfig.class, BaseWebConfig.class, UploadLoginHelper.class})
 @ComponentScan("com.dber.upload.web")
 public class UploadWebConfig {
 }
---
+++ dber-upload-service/src/main/java/com/dber/upload/web/UploadLoginHelper.java
@@ -0,0 +1,20 @@
+package com.dber.upload.web;
+
+import com.dber.base.entity.Account;
+import com.dber.plat.api.PlatLoginHelper;
+
+/**
+ * <li>修改记录: ...</li>
+ * <li>内容摘要: ...</li>
+ * <li>其他说明: ...</li>
+ *
+ * @author dev-v
+ * @version 1.0
+ * @since 2018/1/27
+ */
+public class UploadLoginHelper extends PlatLoginHelper{
+    @Override
+    protected void addAccount(Account account) {
+
+    }
+}
